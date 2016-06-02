dd1eec6f282dbeb32829fb4788602d3b48cec754
Freeman Man <freeman@ycfreeman.com>
2016-06-02 20:24:08 +1000

    init project

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+
+const PORT = 8080;
+
+dispatcher.setStatic("public");
+
+
+
+
+//Lets use our dispatcher
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
